1c8e6b67cdbc77e50be47119e052f084871c2751
Mohammed Aasif <mohammedaasif@tecnovators.in>
2021-02-12 11:36:05 +0530

    add edit and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,33 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+ 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
+    if @article.save
+      flash[:notice] = "Article was created succesfully"
     redirect_to @article
+    else
+      render 'new'
+    end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+    flash[:notice] = "Article Updated succesfully"
+    redirect_to @article
+    else
+      render "edit"
+    end
+  end
+
+
 end
+
