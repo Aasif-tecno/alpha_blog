58961166f2d993f287c06c38cf1c7f895dcae02e
Mohammed Aasif <mohammedaasif@tecnovators.in>
2021-02-15 17:55:12 +0530

    complete user login and signup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
-
+  before_action :authenticate_user!, expect: [:index, :show]  
   before_action :set_article , only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   
   def show
   end
@@ -10,7 +11,8 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article = Article.new
+    # @article = Article.new
+    @article = current_user.articles.build
   end
 
   def edit
@@ -18,7 +20,9 @@ class ArticlesController < ApplicationController
   end
  
   def create
-    @article = Article.new(article_params)
+    # @article = Article.new(article_params)
+    @article = current_user.articles.build(article_params)
+
     if @article.save
       flash[:notice] = "Article was created succesfully"
       redirect_to @article
@@ -43,6 +47,11 @@ class ArticlesController < ApplicationController
     redirect_to articles_path
   end
 
+  def correct_user
+  @article = current_user.articles.find_by(id: params[:id])
+  redirect_to articles_path, notice: "Not Authorize to edit this article" if @article.nil?
+  end
+
   private
 
   def set_article
@@ -50,7 +59,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, :user_id)
   end
 
 end
